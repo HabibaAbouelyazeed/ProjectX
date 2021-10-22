d220a378984ebc850df116bd41ae5aa4641ab009
HabibaAbouelyazeed <48604366+HabibaAbouelyazeed@users.noreply.github.com>
2021-10-22 08:09:31 +0200

    login page components
    page layout without functionality

--- Front-end-Angular/ProjectX-Front/src/app/app-routing.module.ts
+++ Front-end-Angular/ProjectX-Front/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { SignInPageComponent } from './sign-in-page/sign-in-page.component';
+
+const routes: Routes = [
+  {path: 'signIn', component: SignInPageComponent},
+  //{path: '', redirectTo: '/heroes-list', pathMatch: 'full'}  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
