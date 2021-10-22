1713f968fb9e32d12f5f1855cece6398458c0b0a
YoussefMohamed99 <joe_1999_2010@hotmail.com>
2021-10-22 20:44:14 +0200

    Added the activities component.

--- Front-end-Angular/ProjectX-Front/src/app/home-page/home-page.module.ts
+++ Front-end-Angular/ProjectX-Front/src/app/home-page/home-page.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AboutComponent } from './about/about.component';
 import { ActivitiesComponent } from './activities/activities.component';
+import { MatCardModule } from '@angular/material/card';
 
 
 
@@ -11,7 +12,8 @@ import { ActivitiesComponent } from './activities/activities.component';
     ActivitiesComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    MatCardModule
   ],
   exports: [
     AboutComponent,
