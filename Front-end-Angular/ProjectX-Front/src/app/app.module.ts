993f5a13602a0b6e8d0ef3e3f56db0ee0bdacdee
HabibaAbouelyazeed <48604366+HabibaAbouelyazeed@users.noreply.github.com>
2021-10-23 18:32:25 +0200

    fix signup page fix

--- Front-end-Angular/ProjectX-Front/src/app/app.module.ts
+++ Front-end-Angular/ProjectX-Front/src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { ReactiveFormsModule } from '@angular/forms';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -29,7 +31,8 @@ import { SignUpPageComponent } from './sign-up-page/sign-up-page.component';
     AppRoutingModule,
     HomePageModule,
     CommunityPageModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
